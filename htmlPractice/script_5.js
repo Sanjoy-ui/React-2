d324738bf7e81c2586c977cf340c2b4abca1e805
Sanjoy Deb <sanjoydeb404@gmail.com>
2025-07-18 13:53:34 +0530

    practice 5

---
+++ htmlPractice/script_5.js
@@ -0,0 +1,27 @@
+// ques 1 
+
+let number = Math.floor(Math.random()* 6 + 1)
+
+// console.log(number);
+
+// ques 2
+
+const carDetails = {
+    "name" : "bmw",
+    model: "m3" ,
+    color : "black"
+}
+const {color} = carDetails
+// console.log(carDetails["name"]);
+// console.log(carDetails.model);
+// console.log(color);
+
+const person = {
+    name : "sanjoy deb",
+    age : 19 ,
+    city : "teliamura , tripura"
+}
+person.city = "kamalghat"
+person.country = "India"
+
+// console.log(person);
