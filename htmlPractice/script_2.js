7ba9526ed553be0d424b2c1b8a5d985e5c7139ce
Sanjoy Deb <sanjoydeb404@gmail.com>
2025-07-18 12:49:25 +0530

    practice 3

---
+++ htmlPractice/script_2.js
@@ -0,0 +1,65 @@
+// part 3 
+
+// ques 1
+const arr = [19,20 ,30 ,40 ,100 ,50];
+const n =3;
+
+
+const arr2 = [];
+
+for(let i = 0 ; i < n ; i++  ){
+    arr2.push(arr[i])
+}
+
+// console.log(arr2);
+
+// ques 2
+
+const dearr = [1,2,3,4,5,6,7,8]
+const dummyarr =[]
+// .slice(-3)
+
+for (let i =dearr.length - 3; i < dearr.length; i++) {
+   
+        // dummyarr.push(dearr[dearr.length - i])
+        dummyarr.push(dearr[i])
+}
+
+// console.log(dummyarr);
+
+// ques 3
+
+const string = " "
+
+if(!string){
+    // console.log('true');
+} else {
+    // console.log('string is not empty');
+}
+
+
+// ques 4
+
+const char = "SAAHJd"
+
+for (let i = 0; i < char.length; i++) {
+   if (char[i] === char[i].toLowerCase()) {
+    // console.log("lowercase exist" , char[i]);
+   }
+    
+}
+
+// ques 5
+const dummystring = "       sanjoyy.    "
+
+// console.log(dummystring.trim());
+
+// ques 6
+
+const testArray = [1,2,3,45,5,6,];
+
+let gg = 6 
+ 
+if (testArray.includes(gg)) {
+    console.log('exists');
+}
