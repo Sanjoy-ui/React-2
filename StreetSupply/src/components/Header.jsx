5faed8e4e893b95a5f0e4b2a8b4ffdfdb5ea9457
Sanjoy Deb <sanjoydeb404@gmail.com>
2025-08-04 22:26:59 +0530

    ghh..

--- StreetSupply/src/components/Header.jsx
+++ StreetSupply/src/components/Header.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import '../assets/head.css'
 
 
 function Header() {
   return (
-    <div>
-        <div>
-            
-        </div>
-    </div>
+    <>
+        <nav className='bg-green-300 w-full h-16 flex justify-around items-center '>
+            <div className='' >
+                <h1 className='logo right-10 font-bold text-3xl  ' >LOGOmine</h1>
+            </div>
+            <div >
+              <ul className=' ul list-none flex gap-2 font-semibold'>
+                <li><a className='hover:bg-green-400 px-4 py-1.5 rounded-xl hover:shadow-lg' href="">Home</a></li>
+                <li><a className='hover:bg-green-500 px-4 py-1.5 rounded-xl hover:shadow-lg' href="">Blog</a></li>
+                <li><a className='hover:bg-green-500 px-4 py-1.5 rounded-xl hover:shadow-lg' href="">About</a></li>
+                <li><a className='hover:bg-green-500 px-4 py-1.5 rounded-xl hover:shadow-lg' href="">Contact</a></li>
+                <li><a className='burger hover:bg-green-500 px-4 py-1.5 rounded-xl hover:shadow-lg' href="">Menu</a></li>
+                
+              </ul>
+              
+            </div>
+        </nav>
+    </>
   )
 }
 
